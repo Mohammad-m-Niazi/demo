d3256ba994b043174d5e4181c78740f62818bae9
Mohammad Mahdi Niazi <66867944+Mohammad-m-Niazi@users.noreply.github.com>
2026-09-17 17:59:34 -0400

    feat: add skills and technologies section (#12)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import Projects from './components/Projects/Projects'
+import Skills from './components/Skills/Skills'
 function App() {
   return (
     <main className="portfolio" style={{ '--portfolio-background': `url(${backgroundImage})` }}>
@@ -13,6 +14,7 @@ function App() {
           <Hero />
           <About />
           <Projects />
+          <Skills />
         </div>
       </div>
     </main>
